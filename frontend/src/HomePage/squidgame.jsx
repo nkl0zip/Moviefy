9b18217329f9fcf7d09b463931c42f7d30874dd0
nkl0zip <156062676+nkl0zip@users.noreply.github.com>
2025-03-22 23:09:52 +0530

    changed background in home

--- frontend/src/HomePage/squidgame.jsx
+++ frontend/src/HomePage/squidgame.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import "./squidgame.css";
 import char1 from "../images/template1/char1.png";
 import title1 from "../images/template1/title1.png";
+import backgroundImage from "../images/template1/background1.png";
 
 const Squidgame = () => {
+  const backgroundStyle = {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backgroundImage})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    minHeight: "100vh",
+    paddingTop: "170px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  };
   return (
-    <div className="home-page-body">
+    <div style={backgroundStyle} className="home-page-body">
       <div className="text-message-first">EXPLORE A WORLD WHERE GAMES</div>
       <div className="character-title">
         <img className="character" src={char1} alt="" />
